3cbc4485c8736d15d2d93f6ef49a1a4733a06e54
sixftallstar314 <brandon.d.garrett11@gmail.com>
2024-12-05 13:59:22 -0500

    updated server for sessions, also deleted node-modules due to error, currently pushing to save work and redownload packages

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,18 @@
 const express = require('express');
 const bodyParser = require ('body-parser');
-const session = require('express-session');
-const connectKnexSession = require('connect-session-knex')(session);
-const cors = require('cors');
-const knexConfig = require('./knexfile.js');
-const knex = require ('knex')(knexConfig)
+const cookieParser = require('cookie-parser');
+const knex = require ('knex')('./knexfile');
+const cors = require ('cors');
 
-const app = express(); 
+
+const app= express();
 const PORT = 3001;
 
-const store = new connectKnexSession({
-    knex: knex,
-    tablename: 'sessions',
-});
-
-app.use(KnexSession({
-    secret: '20755',
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-    cookie: {
-        maxAge: 24 * 60 * 60 * 1000
-    }
-}));
-
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+const session = {};
 
 knex.schema.hasTable('users').then((exists) => {
     if (!exists) {
@@ -42,7 +28,7 @@ knex.schema.hasTable('items').then((exists) => {
     if (!exists) {
         return knex.schema.createTable('items', (table)=> {
             table.increments('id').primary();
-            table.string('name').notNullable
+            table.string('name').notNullable();
             table.text('description');
             table.integer('quantity').defaultTo(0);
             table.integer('user_id').references('id').inTable('users');
@@ -59,6 +45,7 @@ app.post('/register', (req, res) => {
     .then((id) => res.json({ id: id[0] }));
 });
 
+
 app.post('/login', (req, res) => {
     const {username, password } =req.body
 
@@ -68,15 +55,27 @@ app.post('/login', (req, res) => {
     .first ()
     .then ((user) => {
         if (!user || user.password !== password) {
-            return res.send();
+            return res.send('Invalid username or password');
         }
-        req.session.user = {id: user.id, username: user.username };
-        res.send('Login Good')
+        const sessionId = new Date().getTime();
+        session[sessionId] = {userId: user.id};
+        res.cookie('session_id' , sessionId);
+        res.send('Login successful');
     });
 });
 
+app.use ((req, res, next) => {
+    const sessionId = req.cookies['session_id'];
+    if (session[sessionId]) {
+        req.session = session[sessionId];
+        next();
+    } else {
+        res.send('Failed to login')
+    }
+});
+
 app.get('/items', (reg, res) => {
-    const userId = req.user.id;
+    const userId = req.session.userId;
 
     knex('items')
     .select('*')
@@ -85,17 +84,17 @@ app.get('/items', (reg, res) => {
 
 app.post('/items', (req, res) => {
     const{name, description, quantity} = req.body;
-    const userId = req.user.id;
+    const userId = req.session.userId;
 
     knex('items')
-    .insert({ name, description, quantity})
+    .insert({ name, description, quantity, user_id: userId })
     .returning('*')
     .then((item) => res.json(item[0]))
 });
 
 app.delete('/items/:id', (req, res) => {
     const id = req.params.id;
-    const userId = req.user.id;
+    const userId = req.session.userId;
 
     knex('items')
     .where({ id, user_id: userId })
